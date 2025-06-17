eb012f82f114a9cf3f14f348b75c85926e51078e
Ernesto Caraballo <ernestojcaraballog@gmail.com>
2025-06-17 00:33:45 +0000

    Added an internal refresh so a new card is generated after 10 seconds

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function generateCard() {
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let icons = ["♦", "♥", "♠", "♣"];
+
+  let randomNumb = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomIcons = icons[Math.floor(Math.random() * icons.length)];
+
+  let card = document.querySelector("#center");
+  let domIcon1 = document.querySelector("#pintaUp");
+  let domIcon2 = document.querySelector("#pintaDown");
+
+
+  domIcon1.style.color = "black";
+  domIcon2.style.color = "black";
+
+  if (randomIcons === "♥" || randomIcons === "♦") {
+    domIcon1.style.color = "red";
+    domIcon2.style.color = "red";
+  }
+
+  card.innerText = randomNumb;
+  domIcon1.innerText = randomIcons;
+  domIcon2.innerText = randomIcons;
+}
+
+window.onload = function () {
+  generateCard(); 
+  setInterval(generateCard, 10000); // Actualiza cada 10 segundos
 };
